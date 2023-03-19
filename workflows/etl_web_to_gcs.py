cbc2be739f83683473c95a6cc3da84304501d1b9
Victor Padilha <victorpadilha.cc@gmail.com>
2023-03-19 19:10:00 -0300

    fixing dtype problem in column ZIP CODE

--- workflows/etl_web_to_gcs.py
+++ workflows/etl_web_to_gcs.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
@@ -7,18 +8,30 @@ from prefect_gcp.cloud_storage import GcsBucket
 def extract(dataset_url: str) -> pd.DataFrame:
     """Read NYC MVC data into pandas DataFrame."""
     return pd.read_csv(dataset_url)
-    # return pd.read_csv('dataset.csv')
 
 @task(log_prints=True)
 def transform(data: pd.DataFrame) -> pd.DataFrame:
-    """Basic transformation step. Merges columns CRASH DATE and CRASH TIME into a new column CRASH DATETIME."""
+    """Basic transformation step. Merges columns CRASH DATE and CRASH TIME into a new column CRASH DATETIME and
+    transforms ZIP CODE column to int.
+    """
+    def transform_zipcode(zip_code):
+        if isinstance(zip_code, str):
+            zip_code = zip_code.strip()
+            if zip_code:
+                return int(zip_code.strip())
+            return -1
+        if math.isnan(zip_code):
+            return -1
+        return int(zip_code)
+
     data['CRASH DATETIME'] = data['CRASH DATE'] + ' ' + data['CRASH TIME']
     data['CRASH DATETIME'] = pd.to_datetime(data['CRASH DATETIME'], format='%m/%d/%Y %H:%M')
+    data['ZIP CODE'] = data['ZIP CODE'].apply(transform_zipcode)
     data.drop(['CRASH DATE', 'CRASH TIME'], axis=1, inplace=True)
     return data
 
 @task()
-def write_local(data: pd.DataFrame, data_dir: str, dataset_filename='nyc_mvc') -> Path:
+def write_local(data: pd.DataFrame, data_dir: str = 'raw', dataset_filename: str = 'nyc_mvc') -> Path:
     """Write DataFrame as a parquet file."""
     Path(data_dir).mkdir(parents=True, exist_ok=True)
     path = Path(f'{data_dir}/{dataset_filename}.parquet')
@@ -33,10 +46,10 @@ def write_gcs(path: Path) -> None:
 
 @flow()
 def nyc_mvc_etl(dataset_url: str = 'https://data.cityofnewyork.us/api/views/h9gi-nx95/rows.csv') -> None:
-    """Runs the NYC Motor Vehicle Collisions ETL flow. Extracts data from the web, transforms it and loads into GCS."""
+    """Runs the NYC Motor Vehicle Collisions ETL flow. Extracts data from the web, transforms and loads it into GCS."""
     data = extract(dataset_url)
     data = transform(data)
-    path = write_local(data, 'data')
+    path = write_local(data)
     write_gcs(path)
 
 if __name__ == '__main__':
