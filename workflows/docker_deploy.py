743b5561d6eff094cf5a38da4a3c0bfc06a38ed0
Victor Padilha <victorpadilha.cc@gmail.com>
2023-03-25 10:34:19 -0300

    prefect docker deploy

--- workflows/docker_deploy.py
+++ workflows/docker_deploy.py
@@ -1,5 +1,6 @@
 from prefect.deployments import Deployment
 from prefect.infrastructure.docker import DockerContainer
+from prefect.orion.schemas.schedules import CronSchedule
 from web_to_gcs.etl_web_to_gcs import etl_web_to_gcs
 from gcs_to_bq.etl_gcs_to_bq import etl_gcs_to_bq
 
@@ -9,15 +10,17 @@ if __name__ == '__main__':
         flow=etl_web_to_gcs,
         name='web-to-gcs-docker-flow',
         infrastructure=etl_web_to_gcs_container,
-        entrypoint='etl_web_to_gcs.py:etl_web_to_gcs'
+        entrypoint='etl_web_to_gcs.py:etl_web_to_gcs',
+        schedule=CronSchedule(cron='0 0 * * *', timezone='UTC', day_or=True)
     )
     etl_web_to_gcs_deployment.apply()
 
     etl_gcs_to_bq_container = DockerContainer.load('etl-gcs-to-bq')
     etl_gcs_to_bq_deployment = Deployment.build_from_flow(
         flow=etl_gcs_to_bq,
-        name='docker-flow',
+        name='gcs-to-bq-docker-flow',
         infrastructure=etl_gcs_to_bq_container,
-        entrypoint='etl_gcs_to_bq.py:etl_gcs_to_bq'
+        entrypoint='etl_gcs_to_bq.py:etl_gcs_to_bq',
+        schedule=CronSchedule(cron='0 1 * * *', timezone='UTC', day_or=True)
     )
     etl_gcs_to_bq_deployment.apply()
